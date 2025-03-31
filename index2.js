57b76995bdcc39925482277ec0c2cb84656bf2d6
Prince <principe3219@icloud.com>
2025-03-30 18:58:45 -0700

    more practice

--- index2.js
+++ index2.js
@@ -12,3 +12,18 @@ function randomColor() {
     const color = `rgb(${red}, ${green}, ${blue})`
     body.style.backgroundColor = color;
 }
+
+// const body = document.getElementsByTagName("body")[0]
+
+// function changeColor(name) {
+//     body.style.backgroundColor = name;
+// }
+
+// function randomColor() {
+//     const red = Math.round(Math.random() * 255)
+//     const green = Math.round(Math.random() * 255)
+//     const blue = Math.round(Math.random() * 255)
+
+//     const color = `rgb(${red}, ${green}, ${blue})`
+//     body.style.backgroundColor = color;
+// }
